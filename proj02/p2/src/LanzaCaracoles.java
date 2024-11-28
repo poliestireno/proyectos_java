99d7edf3df6ac5e2d4c2ee3acf4f816f4cc24e4d
poliestireno@gmail.com <poliestireno@gmail.com>
2024-11-28 18:07:46 +0100

    Clase 3

--- proj02/p2/src/LanzaCaracoles.java
+++ proj02/p2/src/LanzaCaracoles.java
@@ -6,39 +6,41 @@ import java.util.List;
 
 public class LanzaCaracoles {
     public static void main(String[] args) {
-        
-        String[][] parametros =
-        {
-            {"10","A"},
-            {"1","B"}
+
+        String[][] parametros = {
+                { "10", "B" },
+                { "1", "A" }
         };
 
-        try
-        {
+        try {
             List<Process> listaProcesos = new ArrayList<>();
-            for (String[] param : parametros)
-            {
-                ProcessBuilder pb = new ProcessBuilder("java","-cp","bin","Caracol",param[0],param[1]);
+            for (String[] param : parametros) {
+                ProcessBuilder pb = new ProcessBuilder("java", "-cp",
+                        "/home/gilbert/.config/Code/User/workspaceStorage/5f8a6ee0f4555d5ce3e3da216edb1520/redhat.java/jdt_ws/proyectos_java_16aea38/bin",
+                        "Caracol", param[0], param[1]);
                 Process p = pb.start();
                 listaProcesos.add(p);
-                BufferedReader lectorSalida = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String linea;
-                while((linea = lectorSalida.readLine())!=null)
-                {
-                    System.out.println(linea);
-                }
+
+                new Thread(() -> {
+                    try {
+                        String linea;
+                        BufferedReader lectorSalida = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                        while ((linea = lectorSalida.readLine()) != null) {
+                            System.out.println(linea);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
             }
-            //falta meter hilos y el waitFor para esperar a todos los procesos.
 
+            for (Process pp : listaProcesos) {
+                pp.waitFor();
+            }
 
-
-
-            
+        } catch (IOException | InterruptedException ee) {
+            System.out.println("Ex:" + ee.getMessage());
         }
-        catch (IOException ee)
-        {
-            System.out.println("Ex:"+ee.getMessage());
-        }
-        
+
     }
 }
